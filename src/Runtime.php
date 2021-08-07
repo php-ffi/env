23c3075df2afe5fcd99ac4e7fbdf2931ec14ba97
SerafimArts <nesk@xakep.ru>
2021-08-07 23:01:54 +0300

    Fix assertAvailable method behaviour

--- src/Runtime.php
+++ src/Runtime.php
@@ -39,7 +39,7 @@ final class Runtime
     ): bool {
         $status ??= self::getStatus();
 
-        if ($status->isRuntimeAvailable()) {
+        if (self::isAvailable($status)) {
             return true;
         }
 
